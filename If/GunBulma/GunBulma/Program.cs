8fc64cf7ef9fc1ce76ad53b649ef8065f4090c51
tugcematur <39506647+tugcematur@users.noreply.github.com>
2022-05-16 11:46:50 +0300

    new

---
+++ If/GunBulma/GunBulma/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace GunBulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kaçıncı gündeyiz?");
+            int gun = Convert.ToInt32(Console.ReadLine());
+            
+            
+            if (gun == 1)
+                Console.WriteLine("Bugün Pazartesi");
+            else if(gun == 2){
+
+                Console.WriteLine("Bugün Salı");
+            }
+
+            else if (gun == 3)
+            {
+
+                Console.WriteLine("Bugün Çarşamba ");
+            }
+            else if (gun == 4)
+            {
+
+                Console.WriteLine("Bugün Perşembe");
+            }
+
+            else if (gun == 5)
+            {
+
+                Console.WriteLine("Bugün Cuma");
+            }
+
+            else if (gun == 6)
+            {
+
+                Console.WriteLine("Bugün Cumartesi");
+            }
+
+            else if (gun == 7)
+            {
+
+                Console.WriteLine("Bugün Pazar");
+            }
+
+
+            else
+            {
+                Console.WriteLine("Yanlış gün girdiniz");
+                Main(null);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ If/KahveGiris/KahveGiris/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace KahveGiris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Adınız:");
+            string ad = Console.ReadLine();
+            Console.WriteLine("Merhaba " + ad+ ",kaç yaşındasın?");
+            int yas = Convert.ToInt32(Console.ReadLine());
+            
+            if(yas >= 18)
+                // Console.WriteLine( "Kahveye hoşgeldin!" + ad);
+                Console.WriteLine($"{ad} Kahveye hoşgeldin" );
+            else
+            {
+               // Math.Abs(-10);
+
+                int bekle = 18 - yas;
+                //Console.WriteLine(ad + "Kahveye Giremezsin" + "Daha" +bekle+ "yıl  beklemen gerekir");
+                Console.WriteLine($"{ad} Kahveye giremezsin, daha {bekle} yıl beklemen gerekiyor. " );
+               
+            }
+            Console.ReadLine();
+            Main(null);
+
+
+        }
+    }
+}
