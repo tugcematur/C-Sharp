9a7a1d93eababaa4a556bd7ed503574065f355d5
tugcematur <39506647+tugcematur@users.noreply.github.com>
2022-05-16 11:45:22 +0300

    new

---
+++ Donguler/Donguler/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static int[] sayilar = { 10, 15, 72, 37, 48 };
+        static int sayac = 0;
+        static int toplam = 0;
+        static void Main(string[] args)
+        {
+            BaslikYaz("For Döngüsü");
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                Console.WriteLine(sayilar[i]);
+                toplam += sayilar[i];
+            }
+           
+            ToplamYaz(toplam);
+      
+
+            
+            BaslikYaz("Do While Döngüsü");
+            do
+            {
+
+                Console.WriteLine(sayilar[sayac]);
+                toplam += sayilar[sayac];
+                sayac = sayac + 1;
+            } while (sayac < sayilar.Length);
+
+            ToplamYaz(toplam);
+
+           
+            BaslikYaz("While Döngüsü");
+
+            while (sayac < sayilar.Length)
+            {
+                Console.WriteLine(sayilar[sayac]);
+                toplam += sayilar[sayac];
+                sayac = sayac += 1;
+            }
+            ToplamYaz(toplam);
+
+            
+            BaslikYaz("For Each Döngüsü");
+            foreach (int item in sayilar)
+            {
+                Console.WriteLine(item);
+                toplam += item;
+            }
+            ToplamYaz(toplam);
+        }
+
+        private static void BaslikYaz(string bas)
+        {
+            toplam = 0;
+            sayac = 0;
+            Console.WriteLine("_____________");
+            Console.WriteLine(bas);
+            Console.WriteLine("_____________");
+        }
+
+        private static void ToplamYaz(int top)
+        {
+            Console.WriteLine("*************");
+            Console.WriteLine("Toplam:"+ top);
+            Console.WriteLine("*************");
+        }
+
+    }
+}
